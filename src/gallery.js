56ee5f34236c818856761e92ee5059f20d3a1b0c
komorebi <8154ace@gmail.com>
2022-08-16 22:22:01 +0545

    Create a new system for gallery complete with animation and lightbox

--- src/gallery.js
+++ src/gallery.js
@@ -1,7 +1,7 @@
 const showspace = document.getElementById("showspace")
 
 let fetchImages = async () => {
-    let uri = "https://api.pexels.com/v1/search?query=nature&per_page=30"
+    let uri = "https://api.pexels.com/v1/search?query=nature&per_page=60"
     return await fetch(uri, {
         headers: {
             'Authorization': '563492ad6f91700001000001c0984e68177449b49c68e16fc9adb51d'
@@ -11,24 +11,42 @@ let fetchImages = async () => {
 
 
 let createDom = (images) => {
-    const highlight = document.createElement("div")
-    highlight.className = "highlight"
-    const highlight_image = document.createElement('img')
-    highlight_image.src=images[0].src.original
-    highlight_image.alt=`Picture no ${images[0].photographer}`
-    highlight.appendChild(highlight_image)
-    showspace.appendChild(highlight)
-
+    // create and append dom element and listeners to each image
     const scroller = document.createElement("div")
     scroller.className="scroller"
-    for(var i = 1; i<(images.length-1);i++){
+    for(var i =0; i<(images.length);i++){
         const holder = document.createElement("div")
-        holder.className="scrollerelem"
+        holder.className="card"
         holder.id = images[i].id
         const image = document.createElement("img")
-        image.src=images[i].src.small
-        image.alt=`Picture from ${images[i].photographer}`
+        const info = document.createElement("dom")
+        info.className="info"
+        image.src=images[i].src.medium
+        info.innerHTML=`<p>${images[i].photographer}</p>`
         holder.appendChild(image)
+        holder.appendChild(info)
+        holder.onclick  = (ev) => {
+            if (!ev.currentTarget.highlighted){
+            document.querySelectorAll(".card").forEach((card) => {
+                card.classList.remove("highlight")
+                card.classList.remove("showinfo")
+            })
+            ev.currentTarget.classList.add("highlight")
+            ev.currentTarget.classList.add("showinfo")
+            ev.currentTarget.highlighted = true
+            //scroll into view
+            //ev.currentTarget.scrollIntoView({behavior: "smooth", block: "center"}) 
+            const space = document.getElementById("showspace")
+            const tgtOffset = ev.currentTarget.offsetLeft - (space.offsetWidth/2)+200;
+            space.scrollTo({top:0,left: tgtOffset, behavior: "smooth"})
+            
+        } else {
+            // fix for hover
+            ev.currentTarget.classList.remove("highlight")
+            ev.currentTarget.classList.remove("showinfo")
+            ev.currentTarget.highlighted = false
+        }
+        }
         scroller.appendChild(holder)
     }
     showspace.appendChild(scroller)
@@ -36,7 +54,6 @@ let createDom = (images) => {
 
 // setup gallery
 fetchImages().then((photos => {
-    console.log(photos)
     createDom(photos)
 }))
 
